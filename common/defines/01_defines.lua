e1e0ec9202c934c282539daa46140cdb2c09d0eb
Argeddion/Lukas <49621165+Ageddion@users.noreply.github.com>
2022-11-18 14:30:55 +0100

    Air-supply fixes

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -214,3 +214,4 @@ NDefines.NMilitary.ARMY_FUEL_COST_MULT = 0 --fuel multiplier for all army missio
 
 --NAir
 NDefines.NAir.FUEL_COST_MULT = 0 --fuel multiplier for all air missions
+NDefines.NAir.MIN_PLANE_COUNT_PARADROP = 1 --Actually allow Airplanes to Paradrop
