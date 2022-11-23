7e8cca127642d3f9b4fe07b93020ee76334d1d56
Argeddion/Lukas <49621165+Ageddion@users.noreply.github.com>
2022-11-23 10:01:34 +0100

    Test-response fixes

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -26,13 +26,6 @@ NDefines.NCountry.BASE_FUEL_GAIN = 0							-- base amount of fuel gained hourly,
 NDefines.NCountry.BASE_FUEL_CAPACITY = 0.1						-- base amount of fuel capacity
 
 NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.3
-NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
-NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
-NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04,	0.06,	0.08,	0.1,	0.14,	0.18,	0.22,	0.26,	0.3,	0.39,	0.48,	0.57,	0.66,	0.75,	0.78,	0.81,	0.84,	0.87,	0.9}		-- Experience needed to progress to the next level
-NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
-NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
-
-NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.04			-- basic population growth per year, used for monthly manpower gain
 
 --------------------
 -- AI battleplans --
@@ -214,4 +207,4 @@ NDefines.NMilitary.ARMY_FUEL_COST_MULT = 0 --fuel multiplier for all army missio
 
 --NAir
 NDefines.NAir.FUEL_COST_MULT = 0 --fuel multiplier for all air missions
-NDefines.NAir.MIN_PLANE_COUNT_PARADROP = 1 --Actually allow Airplanes to Paradrop
+NDefines.NAir.MIN_PLANE_COUNT_PARADROP = 10
